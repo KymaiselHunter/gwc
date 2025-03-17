c2a35947a22f2b509658ab6a6919818c216c388e
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-03-17 06:29:43 -0400

    general progress for the main content, background covers the site properly, starting on grid for website greetings

--- gwc-website/src/App.jsx
+++ gwc-website/src/App.jsx
@@ -10,25 +10,35 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='min-h-screen min-w-screen'
+    <div className='min-h-screen w-screen bg-cover'
       style={{backgroundImage:  `url(${Background})`}}
     >
       <Navbar></Navbar>
-      <div className='flex flex-col h-[2000px]'>
+      <Main></Main>
+    </div>
+  )
+}
+
+function Main(){
+  return(
+    <>
+      <main className='flex flex-col justify-start items-center w-full px-6 
+      py-20 md:py-32 lg:py-40
+      '>
         
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
+        <div className='grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8 place-items-center'>
+          <a href="https://vite.dev" target="_blank" >
+            <img src={viteLogo} className="w-80 h-80" alt="Vite logo" />
           </a>
           <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
+            <img src={reactLogo} className="w-80 h-80" alt="React logo"  />
           </a>
         </div>
         <h1>Vite + React</h1>
         <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
+          {/* <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
-          </button>
+          </button> */}
           <p>
             Edit <code>src/App.jsx</code> and save to test HMR
           </p>
@@ -36,9 +46,9 @@ function App() {
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
-      </div>
-    </div>
-  )
+      </main>
+    </>
+  );
 }
 
 
