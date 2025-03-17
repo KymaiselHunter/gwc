b9d07a85c8b39e2fa5b4660e6f3ae049a2d41254
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-03-17 05:27:37 -0400

    got a template for a navbar

--- gwc-website/src/App.jsx
+++ gwc-website/src/App.jsx
@@ -2,38 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
+import Background from './assets/koa5.jpg'
+
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col min-h-screen min-w-screen bg-white'>
-      <header className='sticky top-0 z-max w-screen backdrop-blur-lg bg-backround/80 shadow-sm'> 
-        <p className='h-16'>etwr</p>
-      
-      </header>
-      <div className='h-[2000px]'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className='min-h-screen min-w-screen'
+      style={{backgroundImage:  `url(${Background})`}}
+    >
+      <Navbar></Navbar>
+      <div className='flex flex-col h-[2000px]'>
+        
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
+
 
 export default App
---
+++ gwc-website/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Logo from '../assets/gwc-logo-rectangle.jpg'
+
+function Navbar()
+{
+  return(
+    <>
+      <header className='sticky top-0 z-max w-screen backdrop-blur-lg bg-backround/80 shadow-sm      '> 
+        <div className='container flex justify-between items-center h-16 p-8'>
+
+          
+          <img src={Logo} alt="GirlsWhoCode@Hunter"
+            className='h-16'
+          ></img>
+          <p>ddsfetwr</p>
+          <p>reah</p>
+
+        </div>
+
+      </header>
+    </>
+  );
+} export default Navbar
