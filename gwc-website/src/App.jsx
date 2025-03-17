c8fb26f052b23d7519c3398d99da9a4e3b5ec0e4
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-03-16 23:31:22 -0400

    started project, readied npm and tailwind

---
+++ gwc-website/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex flex-col min-h-screen min-w-screen bg-white'>
+      <header className='sticky top-0 z-max w-screen backdrop-blur-lg bg-backround/80 shadow-sm'> 
+        <p className='h-16'>etwr</p>
+      
+      </header>
+      <div className='h-[2000px]'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
